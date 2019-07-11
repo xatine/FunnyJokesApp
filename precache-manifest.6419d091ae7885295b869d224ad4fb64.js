c2cf6de69327012e9dc1065185b98c302cdc2c14
AdaWarczak <ada@coderscrew.pl>
2019-07-11 14:32:22 +0200

    Updates

---
+++ precache-manifest.6419d091ae7885295b869d224ad4fb64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64730d702802560574540b8d2a718e31",
+    "url": "/FunnyJokesApp/index.html"
+  },
+  {
+    "revision": "f56755ec7ea2a9c84dc2",
+    "url": "/FunnyJokesApp/static/css/main.d00665d9.chunk.css"
+  },
+  {
+    "revision": "13feb47b325b5fdae47c",
+    "url": "/FunnyJokesApp/static/js/2.c3053585.chunk.js"
+  },
+  {
+    "revision": "f56755ec7ea2a9c84dc2",
+    "url": "/FunnyJokesApp/static/js/main.53a4cc1c.chunk.js"
+  },
+  {
+    "revision": "fe05b70d73df795f0cf6",
+    "url": "/FunnyJokesApp/static/js/runtime~main.81e5580d.js"
+  }
+]);
